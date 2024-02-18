072555ebe0329fb944ff8a1830daa5e700d33446
szymonjozwiakzip <jozwiakszymon03@gmail.com>
2024-02-18 17:38:11 +0100

    React-app initialize

---
+++ landing-page/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Moja Aplikacja React</h1>
+      <p>Witaj, Świecie!</p>
+    </div>
+  );
+}
+
+export default App;
